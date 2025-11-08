34de3f5e88804fd6c7daf05d1b9a48fda638444d
Vendula Konečná <VendaKonecna@gmail.com>
2025-11-08 15:17:30 +0100

    add page main

--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -3,11 +3,15 @@ import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import MainSection from "../../components/Section/MainSection"
 
+import { useNavigate } from "react-router-dom"
+
 const MainPage = () => {
+    const navigate = useNavigate()
+
     return <>
             <Header />
             <MainSection> 
-                <Button> Seznam filmů </Button>
+                <Button onClick={() => navigate('/films')}> Seznam filmů </Button>
             </MainSection >
             <Footer />
         </>        

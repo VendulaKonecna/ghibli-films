daa1c3d15862c3956b7608745350988ab6c8e089
Vendula Konečná <VendaKonecna@gmail.com>
2025-11-09 13:33:56 +0100

    pridani react spinner, nacitani dat 3 s

--- src/pages/films/films.page.jsx
+++ src/pages/films/films.page.jsx
@@ -1,24 +1,41 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import AppDataContext from "../../utils/context/addDataContext"
 import Header from "../../components/Header/Header"
 import Section from "../../components/Section/MainSection"
 import FilmCard from "../../components/FilmCard/FilmCard"
 import Footer from "../../components/Footer/Footer"
+import { PuffLoader } from "react-spinners"
 
 const FilmsPage = () => {
-const { appData } = useContext(AppDataContext)
+const { appData } = useContext(AppDataContext);
+const [delayedData, setDelayedData] = useState(null)
+
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setDelayedData(appData)
+  }, 3000)
+
+  return () => clearTimeout(timer)
+}, [appData])
+
+if (!delayedData || !delayedData.films) {
+  return (
+    <div className="loader-container">
+      <PuffLoader color="#109ceb" size={80} />
+    </div>
+  )
+}
 
   if (!appData) {
     return <div>Data nejsou dostupná.</div>;
   }
 
-console.log(appData)
     return (
         <>
             <Header />
             <Section>
                 <main className="films-grid">
-                {appData.films.map((film) => (
+                {delayedData.films.map((film) => (
                 <FilmCard
                     key={film.id}
                     title={film.title}
